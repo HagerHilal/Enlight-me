e217a5d164d38e15f7c473c3361954f27e8ae9ae
hagerhilal <hagerhilal2@gmail.com>
2019-12-08 13:20:31 +0200

    api works

--- Enlightme/app/src/main/java/com/example/android/enlight_me/NetworkUtils.java
+++ Enlightme/app/src/main/java/com/example/android/enlight_me/NetworkUtils.java
@@ -1,6 +1,5 @@
 package com.example.android.enlight_me;
 
-//package com.example.android.datafrominternet.utilities;
 
 import android.net.Uri;
 
@@ -17,15 +16,12 @@ import java.util.Scanner;
 public class NetworkUtils {
 
     final static String bookapi =
-            "GET https://www.googleapis.com/books/v1/volumes" ;
+            "https://www.googleapis.com/books/v1/volumes" ;
     final static String PARAM_QUERY = "q";
 
     public static URL buildUrl(String book) {
         Uri builtUri = Uri.parse(bookapi).buildUpon()
-                .appendQueryParameter("key", "AIzaSyAmhi3tG8hjd_3NrGGV3yL0ggVjcHlzoGY")
                 .appendQueryParameter("q", book)
-                .appendQueryParameter("Content-Type" , "application/json")
-                .appendQueryParameter("Authorization" , "327098358281-u6f2gb56f3i38uhvrnnvdbk1nik800gm.apps.googleusercontent.com")
                 .build();
 
         URL url = null;
