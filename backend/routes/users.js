d4797be0c351a8e47f95597672643fe2df82d0c1
hagerhilal <hagerhilal2@gmail.com>
2019-12-09 20:29:14 +0200

    hi

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -5,64 +5,96 @@ const router = express.Router()
 const validator = require('../Validations/userValidations')
 
 router.use(express.json())
-const User = require('../models/User')
+const USer = require('../models/User')
 
 
-// create a new member and add it to the database
-
-
-
-router.get('/all', async (req, res) => {
-
-  const users = await User.find()
-  res.json({data: users})
-
- 
-});
-
-router.post('/user', async (req,res) => {
-  try {
-   const isValidated = validator.createValidation(req.body)
-   if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })
-  const email = req.body.email
-   var user = await User.findOne({email})
-  if(user) return res.status(400).json({error: 'Email already exists'})
- 
-  const username = req.body.username
-    user = await User.findOne({username})
-  if(user) return res.status(400).json({error: 'username already exists'})
- 
-  
-   const newUser = await User.create(req.body)
-   res.json({msg:'User was created successfully', data: newUser})
-  }
-  catch(error) {
-      console.log(error)
-  }  
-})
-
-
-// router.get("/:id"  , async (req, res) => {
-//   const id = req.params.id;
-//     User.findById(id)
-//     .exec()
-//     .then(r => {
-//       if (r) {
-//         res.status(200).json(r);
-//       } else {
-//         res
-//           .status(404)
-//           .json({ message: "Error" });
-
-//       }
-//     })
-// });
 router.get('/all/:userName', async (req,res) => {
   const username=req.params.userName
   const user = await User.findOne({username})
   if(!user) return res.status(404).send({error: 'User does not exist'})
   res.json({data: user})
-})
+});
+router.get('/all', async (req, res) => {
+
+  const users = await User.find()
+  res.json({data: users})
+
+});
+
+
+
+router.get('/username/:username' , async(req , res) => {
+  const userName  = req.params.username;
+
+  const found = await User.findOne({ "username": userName });
+  if (found)
+    return res.status(400).send(found._id);
+});
+
+
+router.patch('/save/:userid' , async (req , res )=> {
+bookid = req.body.id;
+found = await User.findOne({ "_id": req.params.userid });
+if ( found ){
+  found.saved.push(req.params.bookid);
+  return res.status(400).send(found.saved);
+}
+}
+);   
+
+
+
+
+
+
+
+
+// create a new member and add it to the database
+
+router.post('/', async (req, res) => {
+
+  try {   
+    const isValidated = validator.createValidation(req.body)
+    console.log(req.body)
+    if (isValidated.error) { console.log(isValidated.error.details[0].message)
+      return res.status(400).send({ error: isValidated.error.details[0].message })}
+    const username = req.body.username;
+    const found = await User.findOne({ "username": username });
+    if (found)
+      return res.status(400).send({ error: "username is already in use" });
+    else {
+      const email = req.body.email;
+      const found = await User.findOne({ "email": email });
+      if (found)
+        return res.status(400).send({ error: "email is already in use" });
+      else {
+        const newUser = await User.create(req.body)         
+        res.json({ msg: 'Member was created successfully', data: newUser })
+      }
+    }
+  }
+  catch (error) {
+    // We will be handling the error later
+    console.log(error)
+  }
+
+});
+
+router.get("/:id"  , async (req, res) => {
+  const id = req.params.id;
+    User.findById(id)
+    .exec()
+    .then(r => {
+      if (r) {
+        res.status(200).json(r);
+      } else {
+        res
+          .status(404)
+          .json({ message: "Error" });
+
+      }
+    })
+});
    
 /*
 router.put('/:_id', async (req, res) => {
